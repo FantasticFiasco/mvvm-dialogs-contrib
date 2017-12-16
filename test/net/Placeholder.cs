3809d6e1e92fea8a7e75b64bc1d94af058598de9
FantasticFiasco <mattias.kindborg@gmail.com>
2017-12-16 14:30:11 +0100

    test: add test project

---
+++ test/net/Placeholder.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace MvvmDialogs.ContribTest
+{
+    // TODO: Remove me when the first valid test is written in this assembly
+    public class Placeholder
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.That(1+1, Is.EqualTo(2));
+        }
+    }
+}
