85a0e08ed0f47801b331176bfc5fed45489cd04e
FantasticFiasco <mattias.kindborg@gmail.com>
2017-12-16 15:08:25 +0100

    build for additional .NET versions

--- build/build.ps1
+++ build/build.ps1
@@ -5,5 +5,9 @@ $LOGGER = 'C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll'
 Write-Host 'Restore NuGet packages...' -ForegroundColor Green
 nuget restore $SOLUTION
 
-Write-Host 'Build solution in default configuration...' -ForegroundColor Green
+Write-Host 'Build for default .NET version...' -ForegroundColor Green
 msbuild $SOLUTION /t:Rebuild /p:Configuration=Release /m /logger:$LOGGER
+
+Write-Host 'Build for additinal .NET versions...' -ForegroundColor Green
+msbuild $NETPROJECT /t:Rebuild /p:Configuration=Release;TargetFrameworkVersion=v4.0 /m /logger:$LOGGER
+msbuild $NETPROJECT /t:Rebuild /p:Configuration=Release;TargetFrameworkVersion=v3.5 /m /logger:$LOGGER
