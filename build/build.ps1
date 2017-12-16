19ca8135d46bcbfe4a0d330609fe3a31d2c2a812
FantasticFiasco <mattias.kindborg@gmail.com>
2017-12-16 15:10:51 +0100

    wip

--- build/build.ps1
+++ build/build.ps1
@@ -6,8 +6,8 @@ Write-Host 'Restore NuGet packages...' -ForegroundColor Green
 nuget restore $SOLUTION
 
 Write-Host 'Build for default .NET version...' -ForegroundColor Green
-msbuild $SOLUTION /t:Rebuild /p:Configuration=Release /m /logger:$LOGGER
+msbuild $SOLUTION /t:Rebuild /p:'Configuration=Release' /m /logger:$LOGGER
 
-Write-Host 'Build for additinal .NET versions...' -ForegroundColor Green
-msbuild $NETPROJECT /t:Rebuild /p:Configuration=Release;TargetFrameworkVersion=v4.0 /m /logger:$LOGGER
-msbuild $NETPROJECT /t:Rebuild /p:Configuration=Release;TargetFrameworkVersion=v3.5 /m /logger:$LOGGER
+Write-Host 'Build for additional .NET versions...' -ForegroundColor Green
+msbuild $NETPROJECT /t:Rebuild /p:'Configuration=Release;TargetFrameworkVersion=v4.0' /m /logger:$LOGGER
+msbuild $NETPROJECT /t:Rebuild /p:'Configuration=Release;TargetFrameworkVersion=v3.5' /m /logger:$LOGGER
