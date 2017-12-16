75b1e3288cdfbbf2861aa347c85354c4eeee0f68
FantasticFiasco <mattias.kindborg@gmail.com>
2017-12-16 15:03:36 +0100

    build solution

--- build/build.ps1
+++ build/build.ps1
@@ -2,4 +2,8 @@ $SOLUTION = 'MvvmDialogs.Contrib.sln'
 $NETPROJECT = 'src\net\MvvmDialogs.Contrib.csproj'
 $LOGGER = 'C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll'
 
+Write-Host 'Restore NuGet packages...'
 nuget restore $SOLUTION
+
+Write-Host 'Build solution in default configuration...'
+msbuild $SOLUTION /t:Rebuild /p:Configuration=Release /m /logger:$LOGGER
