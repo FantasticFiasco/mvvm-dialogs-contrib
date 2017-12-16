22d5a28f9fa2b1628a1a1cb7d1fecedec90b362f
FantasticFiasco <mattias.kindborg@gmail.com>
2017-12-16 14:21:53 +0100

    chore: add solution

---
+++ SolutionInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MVVM Dialogs Contrib")]
+[assembly: AssemblyCopyright("Copyright © 2017 MVVM Dialogs contributors")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// These versions are updated by the CI server and shouldn't be updated for
+// each release.
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyInformationalVersion("0.0.1")]
