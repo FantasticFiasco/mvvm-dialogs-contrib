83195ea17e8f75d05761873bd7f4d588ba10a668
FantasticFiasco <mattias.kindborg@gmail.com>
2017-12-16 15:23:07 +0100

    add nuget spec

--- build/build.ps1
+++ build/build.ps1
@@ -11,3 +11,9 @@ msbuild $SOLUTION /t:Rebuild /p:'Configuration=Release' /m /logger:$LOGGER
 Write-Host 'Build for additional .NET versions...' -ForegroundColor Green
 msbuild $NETPROJECT /t:Rebuild /p:'Configuration=Release;TargetFrameworkVersion=v4.0' /m /logger:$LOGGER
 msbuild $NETPROJECT /t:Rebuild /p:'Configuration=Release;TargetFrameworkVersion=v3.5' /m /logger:$LOGGER
+
+Write-Host 'Pack NuGet...'
+Write-Host $APPVEYOR_REPO_BRANCH
+# if ($APPVEYOR_REPO_BRANCH != 'master') {
+#     Write-Host 'Is on master'
+# }
