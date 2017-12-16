fb6dd3e46dbec146c5b0262390f125ce874b701b
FantasticFiasco <mattias.kindborg@gmail.com>
2017-12-16 14:55:59 +0100

    wip

--- build/build.ps1
+++ build/build.ps1
@@ -1,4 +1,4 @@
-$SOLUTION = MvvmDialogs.sln
+$SOLUTION = MvvmDialogs.Contrib.sln
 $NETPROJECT = src\net\MvvmDialogs.Contrib.csproj
 $LOGGER = C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll
 
